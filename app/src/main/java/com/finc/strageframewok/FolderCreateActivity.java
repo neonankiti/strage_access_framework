008e27956640a160d03ec3fe8b37eefd3e63de69
南里勇気 <nankiti@nanriyuuki-no-MacBook-Pro-2.local>
2016-04-09 19:12:55 +0900

    create folder create activity

---
+++ app/src/main/java/com/finc/strageframewok/FolderCreateActivity.java
@@ -0,0 +1,9 @@
+package com.finc.strageframewok;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This is {@link android.app.Activity} for folder creation.
+ */
+public class FolderCreateActivity extends AppCompatActivity {
+}
